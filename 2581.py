8d5827abb73c458d78eae9209547d5af11670a77
sangeunlee2005 <163720465+sangeunlee2005@users.noreply.github.com>
2026-04-09 22:09:30 +0900

    Update 2581.py

--- 2581.py
+++ 2581.py
@@ -5,12 +5,12 @@ prime = []
 for i in range(x, y+1):
     error = 0
     if i > 1 :
-        for j in range(2, i):  # 2부터 num-1까지
+        for j in range(2, i):
             if i % j == 0:
                 error += 1
-                break  # 2부터 num-1까지 나눈 몫이 0이면 error가 증가하고 for문을 끝냄
+                break
         if error == 0:
-            prime.append(i)  # error가 없으면 소수리스트에 추가
+            prime.append(i)
             
 if len(prime) > 0 :
     print(sum(prime))
